4be304ed56614607ee0afddea7b7cb3d11aa6f38
KatrinBnk <yekaterinabanko@gmail.com>
2025-04-01 10:37:10 +0300

    remove token

--- src/configs/api.ts
+++ src/configs/api.ts
@@ -1,5 +1,4 @@
 export const BASE_URL = 'https://front-school-strapi.ktsdev.ru/api';
-export const API_TOKEN = import.meta.env.VITE_STRAPI_API_TOKEN as string;
 
 export const apiEndpoints = {
   recipes: `${BASE_URL}/recipes`,
