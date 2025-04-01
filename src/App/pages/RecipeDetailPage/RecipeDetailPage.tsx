c4a874dc3504eec4ec168749554250d0e7ad098d
KatrinBnk <yekaterinabanko@gmail.com>
2025-04-01 10:36:45 +0300

    move foodInfo to ./constants.ts

--- src/App/pages/RecipeDetailPage/RecipeDetailPage.tsx
+++ src/App/pages/RecipeDetailPage/RecipeDetailPage.tsx
@@ -8,13 +8,7 @@ import FoodInfo from './components/FoodInfo';
 import Summary from './components/Summary';
 import Directions from './components/Directions';
 import NeededProducts from './components/NeededProducts';
-
-interface FoodInfo {
-  label: string;
-  data: string;
-  gridColumn: number;
-  gridRow: number;
-}
+import {getFoodInfo} from './constants.ts'
 
 const RecipeDetailPage: React.FC = () => {
   const { documentId } = useParams<{ documentId: string }>();
@@ -48,44 +42,7 @@ const RecipeDetailPage: React.FC = () => {
   if (error || !recipe)
     return <div className={styles.container}>{error || 'Рецепт не найден'}</div>;
 
-  const foodInfo: FoodInfo[] = [
-    {
-      label: 'Preparation',
-      data: recipe.preparationTime + ' minutes',
-      gridColumn: 1,
-      gridRow: 1,
-    },
-    {
-      label: 'Cooking',
-      data: recipe.cookingTime + ' minutes',
-      gridColumn: 2,
-      gridRow: 1,
-    },
-    {
-      label: 'Total',
-      data: recipe.totalTime + ' minutes',
-      gridColumn: 3,
-      gridRow: 1,
-    },
-    {
-      label: 'Likes',
-      data: recipe.likes.toString(),
-      gridColumn: 1,
-      gridRow: 2,
-    },
-    {
-      label: 'Servings',
-      data: recipe.servings + ' servings',
-      gridColumn: 2,
-      gridRow: 2,
-    },
-    {
-      label: 'Rating',
-      data: recipe.rating + '/5',
-      gridColumn: 3,
-      gridRow: 2,
-    },
-  ];
+  const foodInfo = getFoodInfo(recipe);
 
   return (
     <main className={styles.RecipeDetailPage}>
