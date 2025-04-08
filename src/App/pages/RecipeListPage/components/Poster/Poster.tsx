aab6e193ec30603a78160049f66d46f0bb7ca923
KatrinBnk <yekaterinabanko@gmail.com>
2025-04-08 11:55:34 +0300

    update RecipeListPage/poster

--- src/App/pages/RecipeListPage/components/Poster/Poster.tsx
+++ src/App/pages/RecipeListPage/components/Poster/Poster.tsx
@@ -1,14 +1,28 @@
 import React from 'react';
 import styles from './Poster.module.scss';
 import PosterImage from '/assets/poster.png';
+import Text from '@components/Text';
 
 const Poster: React.FC = () => {
   return (
     <div className={styles['poster']}>
       <img src={PosterImage} alt="Recipes banner" />
       <div className={styles['poster__overlay']}>
-        Find the perfect food and <a>drink ideas</a> for every occasion, from{' '}
-        <a>weeknight dinners</a> to <a>holiday feasts</a>.
+        <Text view="p-20" color="primary">
+          Find the perfect food and{' '}
+          <Text tag="a" view="link">
+            drink ideas
+          </Text>{' '}
+          for every occasion, from{' '}
+          <Text tag="a" view="link">
+            weeknight dinners
+          </Text>{' '}
+          to{' '}
+          <Text tag="a" view="link">
+            holiday feasts
+          </Text>
+          .
+        </Text>
       </div>
     </div>
   );
