281401efb04a419a7e7ae0be6baa9e28652a7773
KatrinBnk <yekaterinabanko@gmail.com>
2025-04-22 12:08:09 +0300

    fix firebaseConfig

--- src/configs/firebaseConfig.ts
+++ src/configs/firebaseConfig.ts
@@ -1,8 +1,8 @@
-import { initializeApp } from 'firebase/app';
+import { initializeApp, type FirebaseOptions } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
 import { getAuth } from 'firebase/auth';
 
-export const firebaseConfig = {
+export const firebaseConfig: FirebaseOptions = {
   apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
   authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
   projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
